1323f82077940da0194597e38844f4db3b9fee33
lassespilling <lassespilling@gmail.com>
2020-03-22 08:12:15 +0100

    move particles to app.js from intro to fix overflow issue

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,6 @@ import Emoji from "./layout/Emoji";
 import "./intro.scss";
 import ScrollAnimation from "react-animate-on-scroll";
 import "animate.css/animate.min.css";
-import C from "./layout/CoronaParticle";
 class Intro extends React.Component {
     componentDidMount() {
         const script = document.createElement("script");
@@ -16,15 +15,7 @@ class Intro extends React.Component {
     }
     render() {
         return (
-            <section id="intro" className="position-relative container w-100">
-            <C speed="500" dir="bottom" size="2" x="80" y="70"/>
-            <C speed="200" dir="bottom" size="10" x="20" y="20"/>
-            <C speed="300" dir="bottom" size="35" x="1" y="35"/>
-            <C speed="500" dir="bottom" size="3" x="5" y="5" blur="1"/>
-            <C speed="400" dir="bottom" size="5" x="65" y="40"/>
-            <C speed="300" dir="bottom" size="15" x="80" y="10" blur="1"/>
-            <C speed="100" dir="bottom" size="25" x="85" y="40" blur="2"/>
-            <C speed="50" dir="bottom" size="15" x="45" y="80"/>
+            <section id="intro" className="container-fluid">
                 <div class="container pt-5 pb-5">
                     <ScrollAnimation animateIn="fadeIn" animateOnce="true">
                         <div class="row text-center pb-5">
--- src/components/layout/CoronaParticle.js
+++ src/components/layout/CoronaParticle.js
@@ -1,13 +1,13 @@
 import React from "react";
 
-const CoronaParticle = ({ speed, dir, id, size, x,y,blur }) => {
+const CoronaParticle = ({ speed, dir, size, x,y,blur }) => {
     return (
         <span
-            className="plax"
+            className="plax position-absolute"
             data-jsuplxdir={dir}
             data-jsuplxperspective={speed}
-        >
-            <div className="coronaparticle" id={`coronaparticle${id}`} style={{top: y+"vh", left: x+"vw", width: size+"vmin", height:size+"vmin", filter: `blur(${blur}px)`}}></div>
+            style={{top: y+"vh", left: x+"vw", width: size+"vmin", height:size+"vmin", filter: `blur(${blur}px)`}}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/220px-SARS-CoV-2_without_background.png" alt="" className="w-100"/>
         </span>
     );
 };

6f0df5f20bf9deb0e3c1bd48a9a2b53050c0cd1c
lassespilling <lassespilling@gmail.com>
2020-03-22 07:07:46 +0100

    revert to old rss card

--- src/components/rss/RssCard.js
+++ src/components/rss/RssCard.js
@@ -22,16 +22,12 @@ function RssCard({ logo, src, title }) {
                                 {title}
                             </h3>
                         </div>
-                        <div className="col-sm-12">
-
-                            <RssList src={src} />
-                        </div>
-                        {/* <div className="col-md-4 d-none d-md-block">
+                        <div className="col-md-4 d-none d-md-block">
                             <RssWall src={src} />
                         </div>
                         <div className="col-md-8">
                             <RssList src={src} />
-                        </div> */}
+                        </div>
                     </div>
                 </div>
             </ScrollAnimation>

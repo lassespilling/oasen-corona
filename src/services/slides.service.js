e69f3e5820aa55166e3d884dea68432ce0c9b608
Ørjan Solli <orjansolli@outlook.com>
2020-03-22 01:48:45 +0100

    Fetch books from cms

--- src/services/slides.service.js
+++ src/services/slides.service.js
@@ -1,7 +1,14 @@
 import data from "./data.json";
 
 export const fetchBooks = () => {
-    return Promise.resolve(data.books);
+    return fetch('https://lk85qrxa.api.sanity.io/v1/graphql/test/default', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({query: "{allBook{title, thumbnail}}"})
+    }).then(r => r.json()).then(d => d.data.allBook)
 };
 export const fetchStreaming = () => {
     return Promise.resolve(data.streaming);

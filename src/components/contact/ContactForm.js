e6a5455b7a259054ee67f1f99066273c492bc23a
lassespilling <lassespilling@gmail.com>
2020-03-24 21:18:23 +0100

    fix iframe height

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -10,7 +10,7 @@ export default () => {
                         title="kontaktskjema"
                         src="https://docs.google.com/forms/d/e/1FAIpQLSdSBRexdioc8LaFEfCYg5GQcsejpBPg_LTcgDxlzJTRrp9R1w/viewform?embedded=true"
                         width="100%"
-                        height="700"
+                        height="800"
                         scrolling="yes"
                         frameborder="0"
                         onload="this.height=this.contentWindow.document.body.scrollHeight;"
